5af30dc6d998ded2fc16cc84bab3a9b74e5b03fb
KevPPC <142306008+KevPPC@users.noreply.github.com>
2024-09-10 16:22:34 +0800

    Create proxy.js

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const response = await axios.post('http://54.254.194.136:183/dog-predict', event.body, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data' })
+    };
+  }
+};
